4b6d50b772a4480a12ef633269d34c3c6ffbf85c
Zachary Cooremans <zcooremans@gmail.com>
2021-03-19 18:45:35 -0500

    some touch ups

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import styled from 'styled-components'
 import axios from 'axios'
 import './App.css';
 import Character from './components/Character'
@@ -21,6 +22,12 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const BigH1 = styled.h1`
+    text-align:center;
+    color:red;
+
+  `
   useEffect(() => {
     axios
     .get((`https://swapi.dev/api/people/`))
@@ -32,11 +39,11 @@ const App = () => {
       console.log(err)
     })
   }, [])
- 
+  
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <BigH1 className="Header">Characters</BigH1>
       {characters.map(e => {
         return <Character key={e.name} character={e}/>
         {/* return <Details key={e.name} character={e} action={openDetails}/> */}
